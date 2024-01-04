902e0cc6570e118478d166c830734937e9461f68
Davi Almeida <davialmeidacont@gmail.com>
2024-01-03 21:20:41 -0300

    Created basic home template

---
+++ README.rd
@@ -0,0 +1,3 @@
+Projeto Bot do Discord
+Crir um Bot que realiza alertas no discord
+As datas serão cadastradas em um banco de dados atraves de aplicação web desenvolvida no django
--- web/views.py
+++ web/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 
 def home(request):
-    return render(request, 'web/pages/home.html')
+    if request.method == "GET":
+        return render(request, 'web/pages/home.html')
+    elif request.method == "POST":
+        event_name = request.POST.get('event_name')
+        event_date = request.POST.get('event_date')
+        event_message = request.POST.get('event_message')
+        return HttpResponse(event_name + event_date + event_message)
+    else:
+        return HttpResponse('Algo deu errado')
+ 
